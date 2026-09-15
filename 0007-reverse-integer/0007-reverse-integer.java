7ac9316cf5257ee304f693fbe0bf57ed43dcbac2
Arunachalam S <arunachalamsenthilkumar06@gmail.com>
2026-09-15 23:49:54 +0530

    Time: 1 ms (99.97%), Space: 42.5 MB (79.89%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+
+        while(x != 0){
+            int digit = x%10;
+            rev = (rev*10) + digit;
+            x /= 10;
+        }
+
+        return (int)rev==rev ?(int) rev:0;
+    }
+}
